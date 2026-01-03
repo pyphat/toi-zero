7db502a6dae0ff3c30e8084582e0a5086a0b1973
Piyaphat Liamwilai <82884260+pyphat@users.noreply.github.com>
2026-01-03 21:48:45 +0700

    add: A1-001

---
+++ A1/A1-001.cpp
@@ -0,0 +1,17 @@
+/**
+* task:     A1-001
+* @author   pyphat
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+    string a, b;
+    cin >> a >> b;
+    cout << "Hello " << a << " " << b << '\n';
+    cout << a[0] << a[1] << b[0] << b[1];
+    return 0;
+}
